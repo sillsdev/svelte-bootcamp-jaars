ecbc633dc6690b4f3e1ae022252ef55670a489b2
Aidan Jones <ogonivanovich@gmail.com>
2022-05-23 12:56:58 -0500

    Simplified View One

--- aidan_jones/src/View_One.svelte
+++ aidan_jones/src/View_One.svelte
@@ -1,10 +1,9 @@
 <script>
     import { count } from './stores.js';
+
 </script>
 
-<main>
-    <h1>View 1</h1>
-    <button on:click={() => count.update(n => n-1)}>-</button>
-    <input type=number bind:value={$count}/>
-    <button on:click={() => count.update(n => n+1)}>+</button>
-</main>
+<h1>View 1</h1>
+<button on:click={() => $count--} >-</button>
+<input type=number bind:value={$count}/>
+<button on:click={() => $count++}>+</button>
