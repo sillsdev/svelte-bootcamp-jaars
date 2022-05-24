51c9d7b9996b232f7e288e3880dd4441b0a8628b
Chris Hubbard <chris@thehubbards.org>
2022-05-24 09:00:30 -0400

    AppBuilder init

---
+++ appbuilder/src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Navbar from "./Navbar.svelte";
+	import Router from 'svelte-spa-router'
+	import routes from "./routes"
+
+	let Pages = [
+		{ url: "/", label: "Home" },
+		{ url: "/#/about", label: "About"},
+		{ url: "/#/search", label: "Search"},
+		{ url: "/#/settings", label: "Settings"},
+		{ url: "/#/notes", label: "Notes"},
+
+	]
+</script>
+
+
+
+<Navbar navlists={Pages}/>
+
+<main>
+	<Router {routes} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		padding-top: 0;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	 
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+</style>
