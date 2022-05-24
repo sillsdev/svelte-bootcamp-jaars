d31ed63dd89f9cba2eeb9d70e6b90a7cc0221bc3
Chris Hubbard <chris@thehubbards.org>
2022-05-24 16:06:43 -0400

    Minimal Routing

---
+++ appbuilder-kit/src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/history">History</a>
+    <a href="/highlights">Highlights</a>
+    <a href="/notes">Notes</a>
+  </nav>
+  
+  <slot></slot>
