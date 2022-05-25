a03ec725c4cb1cdc6eea00af980cd5e81b8f8872
Aidan Jones <ogonivanovich@gmail.com>
2022-05-25 12:32:48 -0500

    Componentized History Card
    -Created HistoryCard component
    -Updated history/index.svelte to use component

--- appbuilder-kit/src/routes/history/index.svelte
+++ appbuilder-kit/src/routes/history/index.svelte
@@ -1,7 +1,65 @@
+<script>
+    import HistoryCard from '$lib/components/HistoryCard.svelte';
+    let history = [
+        {book:"World English Bible",
+            reference:"Genesis 1",
+            date:"Today | 14:49",},
+        {book:"World English Bible",
+            reference:"Genesis 1:1",
+            date:"Today | 14:48",},
+        {book:"World English Bible",
+            reference:"John 1",
+            date:"Today | 11:09",},
+        {book:"World English Bible",
+            reference:"John 1:7",
+            date:"Today | 10:03",},
+        {book:"World English Bible",
+            reference:"John 1",
+            date:"Today | 09:48",},
+        {book:"World English Bible",
+            reference:"John 1:5",
+            date:"Today | 09:48",},
+        {book:"World English Bible",
+            reference:"John 1:1",
+            date:"Today | 09:46",},
+        {book:"World English Bible",
+            reference:"John 1",
+            date:"Today | 09:45",},
+        {book:"World English Bible",
+            reference:"John 1:3",
+            date:"Today | 09:43",},
+        {book:"World English Bible",
+            reference:"John 1",
+            date:"Today | 09:43",},
+        {book:"World English Bible",
+            reference:"John 1:3",
+            date:"Today | 09:42",},
+        {book:"World English Bible",
+            reference:"John 1",
+            date:"Yesterday | 15:36",},
+        {book:"World English Bible",
+            reference:"John 1:3",
+            date:"Yesterday | 15:30",},
+        {book:"World English Bible",
+            reference:"John 1",
+            date:"22 May 2022 | 13:48",},
+        {book:"World Messianic Bible",
+            reference:"Yochanan 1",
+            date:"22 May 2022 | 13:42",},
+        {book:"World English Bible",
+            reference:"John 1",
+            date:"22 May 2022 | 13:39",},];
+</script>
+
 <h1>History</h1>
 
 <body class="history">
-    <div id="15" class="history-item-block">
+    {#each history as h}
+        <HistoryCard {...h} />
+    {/each}
+
+<!--Original HTML-->
+<!--<div id="15" class="history-item-block">
         <div class="history-item-book-collection">World English Bible</div>
         <div class="history-item-reference history-item-reference-bcC01 history-item-reference-bcC01-bkGEN">
             Genesis 1
@@ -175,5 +233,5 @@
             }
         }
     
-    </script>  
+    </script>-->  
 </body> 
