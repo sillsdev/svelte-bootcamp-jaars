ccf6b586a6d7572684439c536b4aad0f513df1eb
davidmoore1 <david_moore1@sil.org>
2022-05-25 09:46:31 -0400

    Implemented about box in View2

--- david-moore/src/View2.svelte
+++ david-moore/src/View2.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
+    async function getAboutText() {
+        const res = await fetch('./about.html');
+        const text = await res.text();
+
+        if (res.ok) {
+            return text;
+        } else {
+            throw new Error(text);
+        }
+    }
+    let promise = getAboutText();
 
 </script>
 
     <h2> View 2 </h2>
 
+     {#await promise}
+        <p>Loading data</p>
+    {:then aboutText}
+       <p> {@html aboutText} </p>
+    {:catch error}
+	    <p style="color: red">{error.message}</p>
+     {/await}
 
+     
 <style>
-    	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
