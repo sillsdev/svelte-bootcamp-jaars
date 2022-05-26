711b389e1711c763c30ea206965e1b069188963f
Aidan Jones <ogonivanovich@gmail.com>
2022-05-26 11:53:38 -0500

    Added CardMenu component
    -Added CardMenu component
    -Updated ColorCard and IconCard to use new component.

--- appbuilder-kit/src/lib/components/ColorCard.svelte
+++ appbuilder-kit/src/lib/components/ColorCard.svelte
@@ -1,31 +1,16 @@
 <script>
+    import CardMenu from './CardMenu.svelte';
     export let reference = "";
     export let text = "";
     export let date = "";
     export let actions = [""];
     export let highlight_color = "1";
-    let showMenu = false;
 </script>
 
 <div class="annotation-item-block">
     <div class="annotation-item-color {"hlp"+highlight_color} annotation-float-left"></div>
     <!--Menu-->
-    <div class="annotation-item-menu annotation-float-right">
-        <span on:click={() => showMenu = !showMenu} class="dropbtn">
-            <img class="dropbtn-image" src="ic_dots_vertical_24_black.png" alt="vertical dots">
-        </span>
-        <div class="dropdown-content {showMenu? "show":"hidden"}">
-            {#each actions as a}
-            <!-- svelte-ignore a11y-missing-attribute-->
-                <a>{a}</a>
-            {/each}
-            <!--Orignal menu. Handle actual actions later-->
-            <!--<a href="I-0">View</a>
-            <a href="E-0">Edit</a>
-            <a href="S-0">Share</a>
-            <a href="D-0">Delete</a>-->
-        </div>
-    </div>
+    <CardMenu actions={actions}/>
     <!--Content-->
     <div class="annotation-item-content-block">
         <div class="annotation-item-reference annotation-item-reference-bcC01 annotation-item-reference-bcC01-bkJHN">{reference}</div>
