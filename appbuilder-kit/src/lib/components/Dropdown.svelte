0545b5d99b8baaf00173064af0fc5e39c2c28da6
Aidan Jones <ogonivanovich@gmail.com>
2022-05-27 18:27:52 -0500

    Added SelectGrid

--- appbuilder-kit/src/lib/components/Dropdown.svelte
+++ appbuilder-kit/src/lib/components/Dropdown.svelte
@@ -3,7 +3,7 @@
     <label tabindex="0" class="btn btn-ghost p-0.5 no-animation">
         <slot name="label"/>
     </label>
-    <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <div tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100">
         <slot name="content"/>
-    </ul>
+    </div>
 </div>
