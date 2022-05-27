d199b32897e157b23786161c23eae1444f8c5491
Aidan Jones <ogonivanovich@gmail.com>
2022-05-27 18:03:24 -0500

    Componentized Navbar
    -Created Navbar.svelte

--- appbuilder-kit/src/routes/__layout.svelte
+++ appbuilder-kit/src/routes/__layout.svelte
@@ -1,47 +1,10 @@
 <script>
     import "../tailwind.css";
-    import HamburgerIcon from "$lib/icons/HamburgerIcon.svelte";
-    import MuteIcon from "$lib/icons/MuteIcon.svelte";
-    import SearchIcon from "$lib/icons/SearchIcon.svelte";
-    import DropdownIcon from "$lib/icons/DropdownIcon.svelte";
-    let book = "John";
-    let chapter = "1";
+    import Navbar from "$lib/components/Navbar.svelte";
+    let reference = {book: "John", chapter:"1", verse:"1"}
 </script>
 
-<div class="navbar bg-base-100">
-    <div class="navbar-start">
-        <div class="btn btn-ghost p-1">
-            <HamburgerIcon/>
-        </div>
-        <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost p-0.5 no-animation">
-                {book} <DropdownIcon/>
-            </label>
-            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                
-            </ul>
-        </div>
-        <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost p-0.5 no-animation">
-                {chapter} <DropdownIcon/>
-            </label>
-            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-            
-            </ul>
-        </div>
-    </div>
-    <div class="navbar-end fill-base-content">
-        <button class="btn btn-ghost btn-circle">
-            <MuteIcon/>
-        </button>
-        <button class="btn btn-ghost btn-circle">
-            <SearchIcon/>
-        </button>
-        <button class="btn btn-ghost btn-circle">
-            <img class="w-5 h-5" src="icons/ic_action_font_bigger_black.png" alt="hamburger menu"/>
-        </button>
-    </div>
-</div>
+<Navbar {...reference}/>
 
 <nav>
     <a href="/">Home</a>
@@ -53,11 +16,3 @@
 </nav>
  
 <slot></slot>
-
-<style>
-    /*
-    img{
-        width: 28px;
-        height: 28px;
-    }*/
-</style>
