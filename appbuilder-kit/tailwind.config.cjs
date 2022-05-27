e0b66632c9794ec9abfb7365ef901f2b2c4ee92e
Aidan Jones <ogonivanovich@gmail.com>
2022-05-27 11:35:35 -0500

    Added DaisyUI
    -Configured project to use DaisyUI, TailwindCSS, and PostCSS

---
+++ appbuilder-kit/tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  //tailwind typography: https://daisyui.com/docs/layout-and-typography/
+  //daisyui: https://daisyui.com/docs/use/
+  plugins: [require("@tailwindcss/typography"), require("daisyui")]
+};
