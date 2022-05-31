a32943bb75cec85c6c7286cb254598a0969c5a5d
Aidan Jones <ogonivanovich@gmail.com>
2022-05-31 15:12:17 -0500

    First attempt at Tabs menu

--- appbuilder-kit/src/lib/components/Navbar.svelte
+++ appbuilder-kit/src/lib/components/Navbar.svelte
@@ -5,12 +5,18 @@
     import SearchIcon from "$lib/icons/SearchIcon.svelte";
     import DropdownIcon from "$lib/icons/DropdownIcon.svelte";
     import TextAppearanceIcon from "$lib/icons/TextAppearanceIcon.svelte";
+    import TabsMenu from "./TabsMenu.svelte";
     export let book = "";
     export let chapter = "";
+
     let chapters = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+    let verses = [
+        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+        21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+        41,42,43,44,45,46,47,48,49,50,51];
 </script>
 
-<div class="dy-navbar bg-base-100">
+<div class="dy-navbar bg-primary">
     <div class="dy-navbar-start">
         <slot name="drawer-button"/>
         <Dropdown>
@@ -22,7 +28,12 @@
             <svelte:fragment slot="label">
                 {chapter} <DropdownIcon/>
             </svelte:fragment>
-            <SelectGrid slot="content" options={chapters}/>
+            <svelte:fragment slot="content">
+                <TabsMenu options={{
+                    "Chapter":{component:SelectGrid,props:{chapters}},
+                    "Verse":{component:SelectGrid,props:{verses}}
+                }} active="Chapter"/>
+            </svelte:fragment>
         </Dropdown>
     </div>
     <div class="dy-navbar-end fill-base-content">
