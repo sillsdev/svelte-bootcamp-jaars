509892a2fb6e251d2d7c7571b26c10f58fa8722c
Aidan Jones <ogonivanovich@gmail.com>
2022-06-01 15:14:35 -0500

    Added autofocus to search

--- appbuilder-kit/src/routes/search.svelte
+++ appbuilder-kit/src/routes/search.svelte
@@ -11,7 +11,8 @@
 <form>
     <div class="dy-form-control">
         <label class="dy-input-group">
-            <input type="text" placeholder="Search" class="dy-input dy-input-bordered" bind:value={searchText}/>
+            <!-- svelte-ignore a11y-autofocus -->
+            <input autofocus type="text" placeholder="Search" class="dy-input dy-input-bordered" bind:value={searchText}/>
             <button on:click|preventDefault={submit} class="dy-btn">
                 <SearchIcon />
             </button>
