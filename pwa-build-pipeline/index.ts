e28d9c0032e214466780490213ed64d4321b6780
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2022-06-09 13:27:34 -0400

    First version

--- pwa-build-pipeline/index.ts
+++ pwa-build-pipeline/index.ts
@@ -3,35 +3,185 @@ import jsdom from 'jsdom';
 
 /**
  * TODO:
- * Fonts
- * Color themes
- * "Traits" (how are these different from features?)
- * Book collection (bc) features
- * Books
  * Backgroud images?
  * Layouts?
- * Menu localizations?
- * Keys?
  */
 
 const dom = new jsdom.JSDOM(readFileSync('./example.xml').toString());
+const { document } = dom.window;
+type BookCollection = {
+    features: any;
+    books: {
+        id: string;
+        name: string;
+        testament: string; // TODO: better name for OT/NT
+        section: string; // Pentateuch
+        chapters: number;
+        chaptersN: string; // 1-34
+        bd: string;
+    }[];
+    style?: {
+        font: string;
+        textSize: number;
+        lineHeight: number;
+        textDirection: string;
+        numeralSystem: string;
+        verseNumbers: string;
+        languageCode: string;
+        languageName: string;
+    };
+}
+const data: {
+    mainFeatures?: any;
+    fonts?: {
+        name: string;
+        family: string;
+        file: string;
+        fontWeight: string;
+        fontStyle: string;
+    }[];
+    themes?: {
+        name: string;
+        enabled: boolean;
+    }[];
+    defaultTheme?: string;
+    traits?: any;
+    bookCollections?: BookCollection[];
+    translationMappings?: {
+        [key: string]: {
+            [lang: string]: string;
+        }
+    };
+    keys?: string[];
+} = {};
 
-const mainFeatures: any = {};
-const mainFeatureTabs = dom.window.document.querySelector("features[type=main]")?.getElementsByTagName("e");
-if(!mainFeatureTabs) throw new Error("Features tag not found in xml");
-// const parsed = data.parseFromString(xmlInput.toString(), "text/xml");
-for(const tag of mainFeatureTabs) {
+function parseConfigValue(value: any) {
+    if(!value.includes(":") && !isNaN(parseInt(value))) value = parseInt(value);
+    else if (["true", "false"].includes(value)) value = value === "true" ? true : false;
+    else {} // " " split array, string, enum or time
+    return value;
+}
+
+// Features
+const mainFeatureTags = document.querySelector("features[type=main]")?.getElementsByTagName("e");
+if(!mainFeatureTags) throw new Error("Features tag not found in xml");
+data.mainFeatures = {};
+
+for(const tag of mainFeatureTags) {
     try {
         let value: any = tag.attributes.getNamedItem("value")!.value;
-        if(!value.includes(":") && !isNaN(parseInt(value))) value = parseInt(value);
-        else if (["true", "false"].includes(value)) value = value === "true" ? true : false;
-        else {} // " " split array, string, enum or time
-        mainFeatures[tag.attributes.getNamedItem("name")!.value] = value;
+        data.mainFeatures[tag.attributes.getNamedItem("name")!.value] = parseConfigValue(value);
     } catch (e) {
         if(e instanceof ReferenceError) {
             console.error("The main features section did not have the expected attributes `name` and `value`");
         } else throw e;
     }
 }
-console.log(mainFeatures);
-writeFileSync("./config.json",  JSON.stringify({ mainFeatures }));
+console.log(`Converted ${Object.keys(data.mainFeatures).length} features`);
+
+
+// Fonts
+const fontTags = document.getElementsByTagName("fonts")[0].getElementsByTagName("font");
+data.fonts = [];
+
+for(const tag of fontTags) {
+    data.fonts.push({
+        family: tag.attributes.getNamedItem("family")!.value,
+        name: tag.getElementsByTagName("font-name")[0].innerHTML,
+        file: tag.getElementsByTagName("f")[0].innerHTML,
+        fontStyle: tag.querySelector("sd[property=font-style]")!.attributes.getNamedItem("value")!.value,
+        fontWeight: tag.querySelector("sd[property=font-weight]")!.attributes.getNamedItem("value")!.value
+    });
+}
+console.log(`Converted ${data.fonts.length} fonts`);
+
+
+// Color themes
+const colorThemeTags = document.getElementsByTagName("color-themes")[0].getElementsByTagName("color-theme");
+data.themes = [];
+
+for(const tag of colorThemeTags) {
+    data.themes.push({
+        name: tag.attributes.getNamedItem("name")!.value,
+        enabled: tag.attributes.getNamedItem("enabled")?.value === "true"
+    });
+    if(tag.attributes.getNamedItem("default")?.value === "true") data.defaultTheme = data.themes[data.themes.length - 1].name;
+}
+console.log(`Converted ${data.themes.length} themes`);
+
+
+// Traits
+const traitTags = document.getElementsByTagName("traits")[0].getElementsByTagName("trait");
+data.traits = {};
+
+for(const tag of traitTags) {
+    data.traits[tag.attributes.getNamedItem("name")!.value] = tag.attributes.getNamedItem("value")?.value;
+}
+console.log(`Converted ${Object.keys(data.traits).length} traits`);
+
+
+// Book collections
+const booksTags = document.getElementsByTagName("books");
+data.bookCollections = [];
+
+for(const tag of booksTags) {
+    const featuresTags = tag.querySelector("features[type=bc]")?.getElementsByTagName("e")!;
+    const features: any = {};
+    for(const feature of featuresTags) {
+        features[feature.attributes.getNamedItem("name")!.value] = parseConfigValue(feature.attributes.getNamedItem("value")!.value);
+    }
+    const books: BookCollection["books"] = [];
+    const bookTags = tag.getElementsByTagName("book");
+    for(const book of bookTags) {
+        books.push({
+            bd: book.getElementsByTagName("bd")[0].innerHTML,
+            chapters: parseInt(book.getElementsByTagName("ct")[0].attributes.getNamedItem("c")!.value),
+            chaptersN: book.getElementsByTagName("cn")[0].attributes.getNamedItem("value")!.value,
+            id: book.attributes.getNamedItem("id")!.value,
+            name: book.getElementsByTagName("n")[0]?.innerHTML,
+            section: book.getElementsByTagName("sg")[0]?.innerHTML,
+            testament: book.getElementsByTagName("g")[0]?.innerHTML
+        });
+    }
+    const stylesTag = tag.getElementsByTagName("styles-info")[0];
+    const writingSystem = tag.getElementsByTagName("writing-system")[0];
+    data.bookCollections.push({
+        features,
+        books,
+        style: {
+            font: stylesTag.getElementsByTagName("text-font")[0].attributes.getNamedItem("family")!.value,
+            languageCode: writingSystem.attributes.getNamedItem("code")!.value,
+            languageName: writingSystem.getElementsByTagName("display-names")[0].getElementsByTagName("form")[0].innerHTML,
+            lineHeight: parseInt(stylesTag.getElementsByTagName("line-height")[0].attributes.getNamedItem("value")!.value),
+            numeralSystem: stylesTag.getElementsByTagName("numeral-system")[0].attributes.getNamedItem("value")!.value,
+            textDirection: stylesTag.getElementsByTagName("text-direction")[0].attributes.getNamedItem("value")!.value,
+            textSize: parseInt(stylesTag.getElementsByTagName("text-size")[0].attributes.getNamedItem("value")!.value),
+            verseNumbers: stylesTag.getElementsByTagName("verse-number-style")[0].attributes.getNamedItem("value")!.value
+        }
+    });
+}
+console.log(`Converted ${data.bookCollections.length} book collections with [${data.bookCollections.map(x => x.books.length).join(", ")}] books`);
+
+
+// Menu localizations
+data.translationMappings = {};
+const translationMappingsTags = document.getElementsByTagName("translation-mappings")[0].getElementsByTagName("tm");
+
+for(const tag of translationMappingsTags) {
+    const localizations: typeof data.translationMappings.key = {};
+    for(const localization of tag.getElementsByTagName("t")) {
+        localizations[localization.attributes.getNamedItem("lang")!.value] = localization.innerHTML;
+    }
+    data.translationMappings[tag.id] = localizations;
+}
+console.log(`Converted ${Object.keys(data.translationMappings).length} translation mappings`);
+
+
+// Keys
+if(document.getElementsByTagName("keys").length > 0) {
+    data.keys = Array.from(document.getElementsByTagName("keys")[0].getElementsByTagName("key")).map(key => key.innerHTML);
+    console.log(`Converted ${data.keys.length} keys`);
+}
+
+
+writeFileSync("./config.json",  JSON.stringify(data));
