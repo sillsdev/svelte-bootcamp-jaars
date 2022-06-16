9137b0171b0a8e1a871663bad1a91e4a79263fd4
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2022-06-16 18:10:26 -0400

    Fix typescript config errors

--- appbuilder-kit/src/lib/components/CardMenu.svelte
+++ appbuilder-kit/src/lib/components/CardMenu.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { clickOutside } from '$lib/scripts/click_outside.js';
+    import { clickOutside } from '$lib/scripts/click_outside';
     import { createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher();
     export let actions = [""];
