cbd110dac6e3ca9b6e238976e8551b760dbec3ef
Aidan Jones <ogonivanovich@gmail.com>
2022-06-24 10:56:21 -0500

    basic search functionality
    Match whole words does nothing currently

--- appbuilder-kit/src/routes/search.svelte
+++ appbuilder-kit/src/routes/search.svelte
@@ -2,16 +2,43 @@
     import SearchIcon from "$lib/icons/SearchIcon.svelte";
     import { docSet } from "$lib/data/stores";
     import { queryPk } from "$lib/scripts/queryPk";
-    import { queries } from 'proskomma-tools';
+    import { postQueries, queries } from "proskomma-tools";
+
     let searchText = "";
     let matchWholeWords = true;
-    let results = [];
-    function submit() {
-        const bcQuery = queries.searchForBookCodes({
-            text: searchText,
-            docSetId: $docSet
-        });
-        console.log(bcQuery);
+    let searching = false;
+    let passages: any[] = [];
+    $: promise = search(searching)
+    async function search(s = false) {
+        if(!s) return; 
+        if(searchText === "") return;
+
+        const books = postQueries.searchForBookCodesFilter({
+            data: JSON.parse(await queryPk(
+                queries.searchForBookCodesQuery({
+                    text: searchText,
+                    docSetId: $docSet
+                })
+            )).data
+        })
+
+        passages = [];
+
+        for(const book of books) {
+            passages = passages.concat(
+                postQueries.searchForVersesFilter({
+                    data: JSON.parse(await queryPk(
+                        queries.searchForPassagesQuery({
+                            text: searchText,
+                            docSetId: $docSet,
+                            bookCode: book
+                        })
+                    )).data
+                })
+            )
+        }
+        console.log(passages.length)
+        searching = false;
     }
 </script>
 
@@ -21,7 +48,7 @@
         <label class="dy-input-group">
             <!-- svelte-ignore a11y-autofocus -->
             <input autofocus type="text" placeholder="Search" class="dy-input dy-input-bordered" bind:value={searchText}/>
-            <button on:click|preventDefault={submit} class="dy-btn">
+            <button on:click|preventDefault={() => searching = true} class="dy-btn">
                 <SearchIcon _class="fill-base-100"/>
             </button>
         </label>
@@ -33,3 +60,12 @@
         </label>
     </div>
 </form>
+
+{#await promise}
+    searching . . . 
+{:then results}
+    {#each passages as p}
+        <h2>{p.reference} <i>{p.docSetId}</i></h2>
+        <p>{p.text}</p>
+    {/each}
+{/await}
