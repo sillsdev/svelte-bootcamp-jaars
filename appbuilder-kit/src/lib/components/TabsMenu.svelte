8435421edc0dca0ff2a3450cdb7570f068a0af08
Aidan Jones <ogonivanovich@gmail.com>
2022-06-27 14:06:28 -0500

    TabsMenu background variable

--- appbuilder-kit/src/lib/components/TabsMenu.svelte
+++ appbuilder-kit/src/lib/components/TabsMenu.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     export let options: App.TabMenuOptions = {"":{component:"",props:{}},};
     export let active = "";
+    export let colorClass = "bg-primary";
 
     function handleMenuaction(e: CustomEvent) {
         console.log(e.detail.text);
     }
 </script>
 
-<div class="tabs bg-primary">
+<div class="tabs {colorClass}">
     {#each Object.keys(options) as opt}
         <!-- svelte-ignore a11y-missing-attribute -->
         <a on:click={() => active = opt}
