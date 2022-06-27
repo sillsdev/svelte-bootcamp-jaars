b3fabed78231e0a56eff41a830b4a852fedaacae
Aidan Jones <ogonivanovich@gmail.com>
2022-06-27 10:39:24 -0500

    added test texts

--- appbuilder-kit/src/lib/data/docSets/docSets.js
+++ appbuilder-kit/src/lib/data/docSets/docSets.js
@@ -1,4 +1,6 @@
 import {eng_web_jhn} from './eng_web_jhn';
 import {eng_lsv_pent} from './eng_lsv_pent';
+import {eng_web_pent} from './eng_web_pent';
+import {rus_syn_pent} from './rus_syn_pent';
 
-export const docSets = [eng_web_jhn, eng_lsv_pent];
+export const docSets = [eng_web_jhn, eng_lsv_pent, eng_web_pent, rus_syn_pent];
